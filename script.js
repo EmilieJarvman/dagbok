7b612397c2e8499c25d205373666293f951dae35
EmilieJarvman <emilie@jarvman.com>
2021-02-19 10:09:43 +0100

    changes

--- script.js
+++ script.js
@@ -9,20 +9,25 @@
 
 // Datum - Scrollbar där datum finns? Eller input med placeholder som anger hur man skriver datum + regex för att plocka upp det. Date(), eller bara siffror? 
 
-// Inputdiv
-const inputDiv = document.createElement("div");
-inputDiv.id = "inputDiv";
-document.body.appendChild(inputDiv);
+// Div för datum och rubrik
+const headertDiv = document.createElement("div");
+headertDiv.id = "headertDiv";
+document.body.appendChild(headertDiv);
 
 // Datum
 const dateElem = document.createElement("input");
 dateElem.setAttribute("type", "date");
-inputDiv.appendChild(dateElem);
+headertDiv.appendChild(dateElem);
 
 // Rubrik
 const rubrikInputElem = document.createElement("input");
 rubrikInputElem.placeholder = "Rubrik";
-inputDiv.appendChild(rubrikInputElem);
+headertDiv.appendChild(rubrikInputElem);
+
+// Div för text och knapp
+const inputDiv = document.createElement("div");
+inputDiv.id = "inputDiv";
+document.body.appendChild(inputDiv);
 
 // Text
 const textAreaElem = document.createElement("textarea");
